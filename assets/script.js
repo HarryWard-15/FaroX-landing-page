7cf2bee7d71cb452c1cd36d2d5e207e6d49c2396
Harry_Desktop <hward.1508@gmail.com>
2023-10-21 20:35:23 +0800

    more tweaks

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,6 @@
 const card = document.querySelector('.card');
 const shadow = document.querySelector('.shadow');
 const splashPage = document.querySelector('.splash-page');
-const faqQuestion = document.querySelectorAll('.question');
-
-faqQuestion.forEach(el => {
-    el.addEventListener('click', function() {
-        console.log(el.childNodes);
-        let answer = el.childNodes[3];
-        answer.style.display = 'block';
-    })
-});
 
 function animateSplash() {
     setTimeout(() => {splashPage.style.opacity = '0'},3000);
