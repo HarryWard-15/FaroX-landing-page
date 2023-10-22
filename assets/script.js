4186f231bd159edf0c9302582130a64f8c62ab80
Harry_Desktop <hward.1508@gmail.com>
2023-10-22 17:00:34 +0800

    timer working & mobile responsiveness

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 const card = document.querySelector('.card');
 const shadow = document.querySelector('.shadow');
 const splashPage = document.querySelector('.splash-page');
+const bannerTimer = document.querySelector('.remaining-time-banner');
+const heroTimer = document.querySelector('.remaining-time');
+
+var countDownDate = new Date("Dec 1, 2023 00:00:00").getTime();
 
 function animateSplash() {
     setTimeout(() => {splashPage.style.opacity = '0'},3000);
@@ -20,6 +24,21 @@ function spinCard(card) {
         calc += 2;
     },16);
 }
+var timer = setInterval(function() {
+    let now = new Date().getTime();
+    let timeLeft = countDownDate - now;
+
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    let timeString = `${days}:${hours}:${minutes}:${seconds}s`;
+
+    heroTimer.innerHTML = timeString;
+    bannerTimer.innerHTML = timeString;
+},1000);
+
 
 
 spinCard(card);
